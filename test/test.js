50d2990d873c2a7277b7d07ba3bd71f954736ed3
sagiv <sagivoron2150@gmail.com>
2023-05-07 02:27:07 +0300

    added my name

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ var app = require("../server.js");
 describe("GET /", function () {
   it('displays "Hello World!"', function (done) {
     // The line below is the core test of our app.
-    request(app).get("/").expect("Hello World!", done);
+    request(app).get("/").expect("Hello World! my name is sagiv", done);
   });
 });
